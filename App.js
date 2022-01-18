ecbc1f8b6da54e09a1d0cab3fe20893ee79ea878
jenikeerthi723 <97227766+jenikeerthi723@users.noreply.github.com>
2022-01-18 11:10:57 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import './index.css';
+
+
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      userData: [],
+    };
+    console.log('hi...');
+  }
+  componentDidMount() {
+    console.log('hi123');
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((res) => res.json())
+      .then((json) => {
+        this.setState({
+          userData: json,
+        });
+      });
+    console.log(this.state.userData);
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1> Fetch data from an api in react </h1>{' '}
+        {this.state.userData.map((item) => (
+         <ol key={item.id}>
+            <img src='./C:\Users\keerthana\learn react\project\public\images/redcar.jpg' alt=""/>
+            <li> Name:{item.username}</li>
+            <li> Email:{item.email}</li>
+
+          </ol>
+        ))}
+      </div>
+  
+  )
+  
+  }
+}
